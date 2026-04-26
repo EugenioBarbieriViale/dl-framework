588d2d41d1618feb07b926cf1a1292acb87c5f51
Eugenio <eugenio.barbieri.viale@gmail.com>
2026-04-26 15:24:06 +0200

    add example

---
+++ examples/mnist.rs
@@ -0,0 +1,48 @@
+use crate::net::Net;
+use load_mnist::load_data;
+use net::functions::{ActivationFunction, LossFunction};
+
+use std::path::Path;
+
+mod load_mnist;
+mod net;
+
+pub struct Hyperparams {
+    epochs: usize,
+    learning_rate: f64,
+}
+
+impl Hyperparams {
+    pub fn new() -> Self {
+        Hyperparams {
+            epochs: 1000,
+            learning_rate: 1e-2,
+        }
+    }
+}
+
+fn main() {
+    let data =
+        load_data("/home/eu/programming/dl-framework/data/train").expect("Could not load dataset.");
+    println!("MNIST dataset has been loaded");
+
+    let arch = vec![28 * 28, 512, 512, 10];
+
+    let params = Hyperparams::new();
+    let loss_func = LossFunction::CrossEntropy;
+    let mut act_funcs = vec![ActivationFunction::ReLU; 3];
+    act_funcs.push(ActivationFunction::Softmax);
+
+    let mut net = Net::new(arch, act_funcs, loss_func);
+    println!("Neural network has been initialized\n");
+
+    println!("Training has started...");
+    net.train(&data.images, &data.classes, &params);
+    println!("Training ended.");
+
+    let path = Path::new("/home/eu/programming/dl-framework/models/mnist.json");
+    println!("Saving model...");
+    net.save_to(path)
+        .expect(&format!("Failed to save to {:?}", path));
+    println!("Done.");
+}
